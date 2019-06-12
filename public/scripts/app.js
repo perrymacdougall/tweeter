5770af26b5926d99bc0d276d938be8d2068b91dc
Perry <perrymacdougall@gmail.com>
2019-06-12 20:33:40 +0000

    successfully loaded up tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,53 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  // Testing tweetdata
-  const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
 
   // Building my tweet function
   function createTweetElement(data) {
@@ -81,11 +34,9 @@ $(document).ready(function() {
     // Loop through tweets
     for (let x in tweets) {
       let result = createTweetElement(tweets[x]);
-      $('#tweet-container').append(result);
+      $('#tweet-container').prepend(result);
     }
   }
-
-
 
   // Switching form submission to AJAX
   $('#new-tweet-form').on('submit', function(event) {
@@ -93,13 +44,23 @@ $(document).ready(function() {
     let $tweetText = $(this).serialize();
 
     $.ajax({
-      url:'/tweets',
+      url: '/tweets',
       method: 'POST',
       data: $tweetText
     })
 
-  });
+    // Loading new tweets
+    function loadTweets(){
+      $.ajax({
+        url: '/tweets',
+        method: 'GET',
+        success: renderTweets
+      })
+    }
 
+    loadTweets();
+
+  });
 
 
 });
